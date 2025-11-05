083720b448d51b396d365b90ec43ba33d693508b
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-11-05 20:56:46 -0300

    Criando rota para listar clientes

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+
 class ClientController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Client::all(['id','name','email']);
+        return response()->json($products, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+
+Route::get('/client', [ClientController::class, 'index'])->name('client.index');
