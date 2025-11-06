ed28c72975d0e69d292974351c396e1c71219333
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-11-05 21:21:16 -0300

    Criando rota para buscar cliente especifico e listar transações

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use App\Models\Transaction;
 
 class ClientController extends Controller
 {
     public function index()
     {
-        $products = Client::all(['id','name','email']);
-        return response()->json($products, 200);
+        $clients = Client::all(['id','name','email']);
+        return response()->json($clients, 200);
+    }
+
+    public function getClient($id)
+    {
+        $client = Client::find($id, ['id', 'name', 'email']);
+        $client['transactions'] = Transaction::where('clients_id', $client['id'])
+            ->select('id', 'gateways_id', 'external_id', 'status', 'amount', 'card_last_numbers')
+            ->orderBy('created_at')
+            ->get();
+        return response()->json($client, 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 
 Route::get('/client', [ClientController::class, 'index'])->name('client.index');
+Route::get('/client/{id}', [ClientController::class, 'getClient'])->name('client.index');
+
+Route::get('/transaction', [TransactionController::class, 'index'])->name('transaction.index');
