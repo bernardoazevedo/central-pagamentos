b0ab8cf2df99a3a6a050c5679cab59898a6051c1
Bernardo Costa <bernardo.costa@doctum.edu.br>
2025-11-13 14:20:14 -0300

    Incluindo seeders em um só arquivo e removendo pacotes não utilizados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Enums\Role;
+use App\Models\Gateway;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,13 +18,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Administrador',
             'email' => 'admin@admin.com',
             'password' => 'admin123',
             'role' => Role::ADMIN,
         ]);
+
+        Gateway::factory()->create([
+            'name' => 'Pagamentos Corp',
+            'class_name' => 'PagamentosCorp',
+            'priority' => 1,
+        ]);
+
+        Gateway::factory()->create([
+            'name' => 'Payments Enterprise',
+            'class_name' => 'PaymentsEnterprise',
+            'priority' => 2,
+        ]);
+
+        // Client::factory(10)->create();
+
+        Product::factory(5)->sequence(
+            ['name' => 'Ball'],
+            ['name' => 'Doll'],
+            ['name' => 'Gaming Console'],
+            ['name' => 'Hat'],
+            ['name' => 'Christmas Tree'],
+        )->create();
     }
 }
